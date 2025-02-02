fca9d8213296cc2e5012ed00842929c37037a3e8
TaranPalSingh18 <taranarora1470@gmail.com>
2025-02-03 03:47:24 +0530

    goodnight

--- Frontend/Blabber_New/src/App.jsx
+++ Frontend/Blabber_New/src/App.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Components/Login'
 import Navbar from './Components/Navbar'
 import Statistics from './Components/Statistics'
 import Footer from './Components/Footer'
 import Hero from './Components/Hero'
+import About from './Components/About'
+import ContactUs from './Components/ContactUs'
+import Signup from './Components/Signup'
+
 
 function App() {
   return (
     <div className='bg-black w-full h-screen flex flex-col'>
-      <Navbar/>
+
+      <Navbar />
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+   
       <Hero />
 
       <Statistics/> 
---
+++ Frontend/Blabber_New/src/Components/ContactUs.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ContactUs() {
+  return (
+    <div>Contactus</div>
+  )
+}
+
+export default ContactUs
